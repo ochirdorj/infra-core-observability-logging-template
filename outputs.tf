3ced704274b72a7601080605fc44c0fd78b06154
Erdenetugs <erdenetugs@gmail.com>
2026-04-12 22:26:43 -0500

    removed the duplicated output

--- outputs.tf
+++ outputs.tf
@@ -19,13 +19,3 @@ output "host_log_group" {
   description = "CloudWatch log group for host logs"
   value       = aws_cloudwatch_log_group.host.name
 }
-
-output "oidc_provider_arn" {
-  description = "ARN of the EKS OIDC provider"
-  value       = aws_iam_openid_connect_provider.eks.arn
-}
-
-output "oidc_provider_url" {
-  description = "URL of the EKS OIDC provider"
-  value       = aws_iam_openid_connect_provider.eks.url
-}
